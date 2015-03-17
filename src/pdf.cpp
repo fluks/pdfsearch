17daa1a03560918dde6caa1c4d24156cd8826324
fluks <kalle.heikola@gmail.com>
2015-03-17 23:16:41 +0200

    Add Pdf class
    Move code about pdfs to its own class.

---
+++ src/pdf.cpp
@@ -0,0 +1,24 @@
+#include "pdf.h"
+#include <poppler-page.h>
+#include <boost/regex.hpp>
+
+std::unique_ptr<std::string>
+Pdfsearch::Pdf::getPage(int i) const {
+    if (i < 0 || i > numberOfPages())
+        throw std::invalid_argument("invalid page number");
+
+    std::unique_ptr<poppler::page> page(doc->create_page(i));
+    if (!page)
+        throw std::runtime_error("can't create page");
+
+    std::vector<char> chars(page->text().to_utf8());
+
+    return std::unique_ptr<std::string>(
+        new std::string(chars.begin(), chars.end()));
+}
+
+bool
+Pdfsearch::Pdf::filenameEndsToPdf(const std::string& file) {
+    static const boost::regex re("\\.pdf$", boost::regex::icase);
+    return boost::regex_search(file, re);
+}
---
+++ src/pdf.h
@@ -0,0 +1,62 @@
+#ifndef PDF_H
+    #define PDF_H
+
+#include <stdexcept>
+#include <string>
+#include <memory>
+#include <poppler-document.h>
+
+namespace Pdfsearch {
+    /** A class for a PDF document.
+     */
+    class Pdf {
+    private:
+        std::string file;
+        std::unique_ptr<poppler::document> doc;
+    public:
+        /** Constructor.
+         * @param file Pdf filename.
+         * @throws std::runtime_error if can't load pdf.
+         */
+        Pdf(const std::string& file) :
+                file(file), doc(poppler::document::load_from_file(file)) {
+            throw std::runtime_error("can't load file");
+        }
+
+        /** No copying. */
+        Pdf(const Pdf& other) = delete;
+        /** No copying. */
+        Pdf&
+        operator=(const Pdf& other) = delete;
+        /** No copying. */
+        Pdf(Pdf&& other) = delete;
+        /** No copying. */
+        Pdf&
+        operator=(const Pdf&& other) = delete;
+
+        /** Get page text.
+         * @param page Page number, [0, numberOfPages()].
+         * @return A pointer to the text in a page.
+         * @throws std::invalid_argument if invalid page parameter given or
+         * if can't create a page.
+         */
+        std::unique_ptr<std::string>
+        getPage(int page) const;
+
+        /** Get number of pages in a pdf.
+         * @return Number of pages.
+         */
+        int
+        numberOfPages() const {
+            return doc->pages();
+        }
+
+        /** Check whether filename ends to '.pdf' case-insensitively.
+         * @param file Filename.
+         */
+        static bool
+        filenameEndsToPdf(const std::string& file);
+    };
+}
+
+#endif // PDF_H
