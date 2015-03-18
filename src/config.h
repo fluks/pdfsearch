084e79a4dde80a477bb3383a3dc7d11a066d4f9e
fluks <kalle.heikola@gmail.com>
2015-03-18 19:42:22 +0200

    Delete Config's constructor
    Before the constructor was private, delete it instead. No reason,
    but to follow the style in other classes.

--- src/config.h
+++ src/config.h
@@ -5,15 +5,15 @@ namespace Pdfsearch {
     /** Program and system specific constants.
      * This class defines constants for the program, such as file paths to
      * resources. make can change these on install.
+     * @note An object of this class cannot be instantiated.
      */
-    class Config {
-    public:
+    struct Config {
+        Config() = delete;
+
         constexpr static const char* VERSION = "0.01";
         constexpr static const char* PROGRAM_NAME = "pdfsearch";
         constexpr static const char* CONFIG_FILE = "src/dummy.conf";
         constexpr static const char* DATABASE_FILE = "pdfsearch.sqlite";
-    private:
-        Config() {};
     };
 }
 
